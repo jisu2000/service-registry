07bc55ebc7e669c137ce5cf1039e2bb37cb3910d
jisu2000 <spjisu2000@gmail.com>
2024-05-26 11:57:56 +0530

    Test api added

--- src/main/java/com/jisu/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/jisu/serviceregistry/ServiceRegistryApplication.java
@@ -1,15 +1,30 @@
 package com.jisu.serviceregistry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+  @GetMapping("/test")
+    public ResponseEntity<?> getHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("Status", "Healthy");
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
 }
